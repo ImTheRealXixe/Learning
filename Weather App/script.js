b39f118b8d8be073ef84d9dbac956f40ea7b132f
ImTheRealXixe <kuba.jarocki1@gmail.com>
2021-12-03 19:55:29 +0100

    functions => class

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,89 +1,87 @@
-let appId = '2a5cc3af57a0f47a0d85d5a5a7e2f7de';
-let units = 'metric';
-let searchMethod ;
 
-function getSearchMethod(searchTerm) {
-    if(searchTerm.lenght === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
-        searchMethod = 'zip';
-    else
-        searchMethod = 'q';
-}
+document.getElementById("searchBtn").addEventListener("click", () => {
+  let searchTerm = document.getElementById("searchInput").value;
+  if (searchTerm) { 
+      weatherApi.getSearchMethod(searchTerm) 
+    };
 
-function searchWeather(searchTerm) {
-    getSearchMethod(searchTerm);
-    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
-        return result.json();
-    }).then(result => {
-        init(result);
-    })
-}
+});
 
-function init(resultFromServer) {
-    console.log(resultFromServer);
-    switch (resultFromServer.weather[0].main) {
-        case 'Clear':
-            document.body.style.backgroundImage = 'url("./img/blue-sky.jpg")';
-            break;
-        
-        case 'Clouds':
-            document.body.style.backgroundImage = 'url("./img/clouds.jpg")';
-            break;
+class weatherApi {
 
-        case 'Rain':
-            document.body.style.backgroundImage = 'url("./img/road-bg.jpeg")';
-            break;
+    constructor() {}
 
-        case 'Thunderstorm':
-            document.body.style.backgroundImage = 'url("./img/lighting-storm.jpg")';
-            break;
-
-        case 'Snow':
-            document.body.style.backgroundImage = 'url("./img/snow.jpeg")';
-            break;
-
-        case 'Drizzle':
-            document.body.style.backgroundImage = 'url("")';
-            break;
-
-        case 'Mist':
-            document.body.style.backgroundImage = 'url("")';
-            break;
-        default:
-            break;
+    static getSearchMethod (searchTerm) {
+        if (searchTerm.lenght === 5 && Number.parseInt(searchTerm) + "" === searchTerm) {
+            weatherApi.getCurrentWeather("zip", searchTerm);
+        } else {
+            weatherApi.getCurrentWeather("q", searchTerm);
+        }
     }
 
-    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
-    let temperatureElement = document.getElementById('temperature');
-    let humidityElement = document.getElementById('humidity');
-    let windSpeedElement = document.getElementById('windSpeed');
-    let cityHeader = document.getElementById('cityHeader');
-    let weatherIcon = document.getElementById('documentIconImg');
+    static getCurrentWeather(searchMethod, searchTerm, units = 'metric', appId = '2a5cc3af57a0f47a0d85d5a5a7e2f7de') {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+            .then(response => {
+                return response.json();
+            })
+            .then(response => {
+                new weatherBox(this.gatherWeatherData(response), document.getElementById("weatherContainer"));
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
-    weatherIcon.src = 'http://openweathermap.org/img/wn/' + resultFromServer.weather[0].icon + '.png';
-
-    let resultDescription = resultFromServer.weather[0].description;
-    weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
-
-    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
-    windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
-    cityHeader.innerHTML = resultFromServer.name;
-    humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
-
-    setPositionForWeatherInfo();
+    static gatherWeatherData(data) {
+        const { weather, main, wind, name } = data
+        const result = {
+            weatherIcon : "http://openweathermap.org/img/wn/" + weather[0].icon + ".png",
+            weatherDescriptionHeader: weather[0].description.charAt(0).toUpperCase() + weather[0].description.slice(1),
+            temperature: Math.floor(main.temp) + "&#176",
+            windSpeed: "Winds at " + Math.floor(wind.speed) + " m/s",
+            cityHeader: name,
+            humidity: "Humidity levels at " + main.humidity + "%",
+            state: weather[0].main
+        }
+        return result
+    }
 }
 
-function setPositionForWeatherInfo() {
-    let weatherContainer = document.getElementById('weatherContainer');
-    let weatherContainerHeight = weatherContainer.clientHeight;
-    let weatherContainerWidth = weatherContainer.clientWidth;
 
-    weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
-    weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/2}px)`;
-    weatherContainer.style.visibility = 'visible';
+class weatherBox {
+
+    constructor(data, container) {
+        this.data = data;
+        this.container = container
+        this.setWeatherData()
+        this.setWeatherContainerPosition()
+    }
+
+    setBackground() {
+        let bg = new Image();
+        let bg_url = `./img/${this.data.state.toLowerCase()}.jpg`
+        bg.src = bg_url
+        bg.onerror = function () {
+            document.body.style.backgroundImage = `url('./img/clear.jpg')`;
+        }
+        bg.onload = function() {
+            document.body.style.backgroundImage = `url(${bg_url})`;
+        }
+    }
+
+    setWeatherData() {
+        for(const [key,value] of Object.entries(this.data)) {
+            try {
+                key != 'weatherIcon' ? document.getElementById(key).innerHTML = value : document.getElementById(key).src = value
+            } catch(err) {
+                key != 'state' ? console.log('Wrong ID') : this.setBackground()
+            }
+         }
+    }
+ 
+    setWeatherContainerPosition() {
+        this.container.style.left = `calc(50% - ${this.container.clientWidth / 2}px)`;
+        this.container.style.top = `calc(50% - ${this.container.clientHeight / 2}px)`;
+        this.container.style.visibility = "visible";
+    }
 }
-
-document.getElementById('searchBtn').addEventListener('click', () => {
-    let searchTerm = document.getElementById('searchInput').value;
-    if(searchTerm)
-    searchWeather(searchTerm);
-})
